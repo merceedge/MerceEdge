c212976a2bcd795387fa32de78cc061abb9c0c82
hobo0cn <hobo0cn@gmail.com>
2019-02-28 17:15:47 +0800

    fix docker run

---
+++ run_demo.sh
@@ -0,0 +1,2 @@
+cp merceedge/config_dev.yaml merceedge/config.yaml 
+edge -f merceedge/tests/formula/rtmp_demo_formula.yaml
---
+++ run_docker_demo.sh
@@ -0,0 +1,2 @@
+cp /home/MerceEdge/merceedge/config_docker.yaml /home/MerceEdge/merceedge/config.yaml 
+python merceedge/__main__.py -f merceedge/tests/formula/rtmp_demo_formula.yaml
