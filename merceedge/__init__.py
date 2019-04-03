359a4c24941812d2ee72751f249b257bee604d57
Halbert Chen <hobo0cn@gmail.com>
2019-04-03 13:17:13 +0800

    data filter demo

--- merceedge/__init__.py
+++ merceedge/__init__.py
@@ -2,8 +2,8 @@ VERSION = (0, 1, 1)
 
 __author__ = 'Halbert Chen'
 __contact__ = "hobo0cn@gmail.com"
-__homepage__ = "http://www.merceproject.com"
+__homepage__ = "http://merceedge.github.io"
 __version__ = ".".join(map(str, VERSION))
-__license__ = "Only allowed for merce group"
+__license__ = "Apache 2.0"
 __apidoc__ = "TODO"
 
--- merceedge/tests/wireload/demo_filter_wireload.py
+++ merceedge/tests/wireload/demo_filter_wireload.py
@@ -1,11 +1,18 @@
 from merceedge.core import WireLoad
 
 class FilterWireLoad(WireLoad):
+    
     name = 'filter_wireload'
     
     def __init__(self, init_params={}):
         super(FilterWireLoad, self).__init__(init_params)
-    
-    def process(self, input_data):
-        # TODO
-        pass
+        self.threshold = init_params.get('threshold')
+
+    async def process(self, input_data):
+        try:
+            value = float(input_data)
+        except ValueError:
+            return
+        
+        if value > self.threshold:
+            await self.put_output_payload(output_name="alert", payload=True)
